e952b1c38b77c5a9e6f127bf1aa38bbbc679e29b
Ken Diehl <diehlkj.99@gmail.com>
2020-09-11 21:14:27 -0400

    oop2.py complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -20,15 +20,26 @@
 class Vehicle():
     pass
 
+
 class FlightVehicle(Vehicle):
     pass
+
+
 class Starship(FlightVehicle):
     pass
+
+
 class Airplane(FlightVehicle):
     pass
+
+
 class GroundVehicle(Vehicle):
     pass
+
+
 class Car(GroundVehicle):
     pass
+
+
 class Motorcycle(GroundVehicle):
     pass
